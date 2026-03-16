6e0974518fac721c614688feb5bcb60b681cf335
Diego <llanesdiego5@gmail.com>
2026-03-16 17:33:05 -0600

    Update gitignore

---
+++ automation-testing/urban-routes-automation/data.py
@@ -0,0 +1,7 @@
+urban_routes_url = 'https://cnt-3b83e8df-9ee9-401e-a2b3-1e36556d3e0d.containerhub.tripleten-services.com?lng=es'
+address_from = 'East 2nd Street, 601'
+address_to = '1300 1st St'
+phone_number = '+1 123 123 12 12'
+card_number, card_code = '1234 5678 9100', '111'
+message_for_driver = 'Muéstrame el camino al museo'
+print(card_number)
